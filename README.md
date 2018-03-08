5bf33f17b9032677a4b7199364ea677d3937f259
PicoJr <ncls_olg@yahoo.fr>
2018-03-08 22:36:20 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Hash Code 2018 Score
+
+**TL;DR** Compute score for Hash Code 2018 Qualification Round.
+
+`score.py` was tested successfully on our final submission.
+
+i.e. we obtained the same scores with `score.py` as those given by the official Judge System.
+
+## Usage
+
+`python3 score.py a_example.in a_example.out`
+
+```
+INFO:root:parsing rides
+INFO:root:opening a_example.in
+INFO:root:3 4 2 3 2 10
+INFO:root:done parsing rides
+INFO:root:parsing a_example.out
+score: 10
+```
+
+`python3 score.py b_should_be_easy.in b_should_be_easy.out`
+
+```
+INFO:root:parsing rides
+INFO:root:opening b_should_be_easy.in
+INFO:root:800 1000 100 300 25 25000
+INFO:root:done parsing rides
+INFO:root:parsing b_should_be_easy.out
+score: 176,877
+```
+
+## Checks
+
+Please note that `score.py` runs very few sanity checks:
+
+it does **not** check:
+
+* ride ids are correct
+* rides are assigned only once
+* number of cars
+* number of steps in the simulation
+
+## Our Team & Qualification Round
+
+<https://hashcode.withgoogle.com/hashcode_2018.html>
+
+Team: MetaModHell
+
+Members (listed alphabetically): Karbok, PicoJr
+
+Score: 46,048,775
+
+Rank: 628th World, 81st France
+
+### Submissions
+
+| Input            |  Score     |
+|:-----------------|-----------:|
+| A-example        | 10         |
+| B-should be easy | 176,877    |
+| C-no hurry       | 13,052,303 |
+| D-metropolis     | 11,353,640 |
+| E-high bonus     | 21,465,945 |
---
+++ score.py
@@ -0,0 +1,98 @@
+#! /usr/bin/python3
+
+import argparse
+import logging
+
+def distance_to_start(position, ride):
+    return abs(position[0] - ride[0]) + abs(position[1] - ride[1])
+
+
+def ride_distance(ride):
+    (x1, y1, x2, y2) = ride[0:4]
+    return abs(x2 - x1) + abs(y2 - y1)
+
+
+def parse_input(file_in):
+    """
+    Parse input file
+    :param file_in: input file name
+    :return: rides_list, rows, columns, vehicles, rides, bonus, steps
+    """
+    logging.info("parsing rides")
+    with open(file_in, 'r') as f:
+        logging.info("opening {}".format(file_in))
+        first_line = f.readline()
+        rows, columns, vehicles, rides, bonus, steps = tuple(map(int, first_line.split(' ')))
+        logging.info("{} {} {} {} {} {}".format(rows, columns, vehicles, rides, bonus, steps))
+        rides_list = []
+        for line in f.readlines():
+            logging.debug(line.strip())
+            ride = tuple(map(int, line.split(' ')))  # x1, y1, x2, y2, step_start, step_end
+            rides_list.append(ride)
+    logging.info("done parsing rides")
+    return rides_list, rows, columns, vehicles, rides, bonus, steps
+
+
+def parse_output(file_out):
+    """
+    Return ride list parsed from output file
+    :param file_out: output file name (solution)
+    :return: vehicle rides, vr[i] == ride list of vehicle i
+    """
+    logging.info("parsing {}".format(file_out))
+    vehicles_rides = []
+    with open(file_out, 'r') as f:
+        for line in f.readlines():
+            rides = list(map(int, line.split(' ')))
+            vehicles_rides.append(rides[1:])  # rides[0] == number of rides
+    return vehicles_rides
+
+
+def compute_score(file_in, file_out):
+    """
+    Compute score (with bonus) of submission
+    :param file_in: input file
+    :param file_out: output file (solution)
+    :return: score
+    """
+    (rides_list, rows, columns, vehicles, rides, bonus, steps) = parse_input(file_in)
+    vehicles_rides = parse_output(file_out)
+    score = 0
+    for vehicle, vehicle_rides in enumerate(vehicles_rides):
+        position = (0, 0)
+        step = 0
+        for rid in vehicle_rides:
+            ride = rides_list[rid]
+            step_min = ride[4]
+            step_max = ride[5]
+            if step + distance_to_start(position, ride) + ride_distance(ride) <= step_max:
+                score += ride_distance(ride)
+                if step + distance_to_start(position, ride) <= step_min:  # bonus
+                    score += bonus
+                step_departure = max(step + distance_to_start(position, ride), step_min)
+                step = step_departure + ride_distance(ride)
+                position = (ride[2], ride[3])
+            else:
+                logging.error("invalid ride")
+    return score
+
+
+def set_log_level(args):
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='print score')
+    parser.add_argument('file_in', type=str, help='file_in')
+    parser.add_argument('file_out', type=str, help='file_out')
+    parser.add_argument('--debug', action='store_true', help='for debug purpose')
+    args = parser.parse_args()
+    set_log_level(args)
+    print("score: {0:,}".format(compute_score(args.file_in, args.file_out)))  # decimal separator
+
+
+if __name__ == '__main__':
+    main()
